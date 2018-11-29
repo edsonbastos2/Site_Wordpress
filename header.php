b6e6be0821bbf175bd4b45950af22ce451d98dc7
Edson Bastos <edsonbastos2@hotmail.com>
2018-11-28 21:26:44 -0300

    Testando Bootstrap colocando stilos com classes bootstrap

--- header.php
+++ header.php
@@ -2,15 +2,20 @@
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Curso WordPress</title>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-	<p>Paragrafo de teste.</p>
 	<header>
 		<section class="top-bar">
-			<div class="social-media-icons">Icones rede social</div>
-			<div class="search">Pesquisa</div>
+			<div class="container">
+				<div class="row">
+					<div class="social-media-icons col-xl-9 col-sm-7 col-6">Icones rede social</div>
+					<div class="search col-xl-3 col-sm-5 col-6 text-rigth">Pesquisa</div>
+				</div>
+			</div>
+			
 		</section>
 
 		<section class="menu-area">
